06b5676e78c81f6c821340c52e9e0d0eb040fdfe
Alex Becerra <abecer02@login-prod-02.pax.tufts.edu>
2024-08-10 04:04:50 -0400

    16x16 grid

--- 2D/mpi4qiskit_all.sh
+++ 2D/mpi4qiskit_all.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
-for i in {0..7}
+# sbatch mpi4qiskit_1.sh 3 100000 0 0
+# for i in {0..7}
+# do
+#     for j in {0..7}
+#     do
+#         sbatch mpi4qiskit_1.sh 3 100000 $i $j
+#     done
+# done
+# for i in {0..15}
+# do
+#     for j in {0..15}
+#     do
+#         sbatch mpi4qiskit_1.sh 4 100000 $i $j
+#     done
+# done
+for i in {0..31}
 do
-    for j in {0..7}
+    for j in {0..31}
     do
-        sbatch mpi4qiskit_1.sh 3 100000 $i $j
+        sbatch mpi4qiskit_1.sh 5 100000 $i $j
     done
 done
-# sbatch mpi4qiskit_1.sh 3 100000 0 0
+# for i in {0..63}
+# do
+#     for j in {0..63}
+#     do
+#         sbatch mpi4qiskit_1.sh 6 100000 $i $j
+#     done
+# done
+# for i in {0..127}
+# do
+#     for j in {0..127}
+#     do
+#         sbatch mpi4qiskit_1.sh 7 100000 $i $j
+#     done
+# done
